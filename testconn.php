24a20806a2e51b91299dc28d4a7b25b5ab22b503
Maxtoq <maxime.toquebiau@hotmail.fr>
2018-05-01 18:54:38 +0200

    tests connexion

---
+++ testconn.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="conn.php" method="post">
+            <table>
+				<tr>
+					<td>Pseudo :</td>
+					<td><input type="text" name="pseudo"></td>
+				</tr>
+				<tr>
+					<td>Mot de passe :</td>
+					<td><input type="text" name="mdp"></td>
+				</tr>
+                <tr>
+                    <td><input type="submit" value="Connexion"></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
