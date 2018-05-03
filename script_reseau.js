c1541cb78907fdad0e2aacf797d39b721059e77c
Thomas WUHRLIN <thomas.wuhrlin@edu.ece.fr>
2018-05-03 18:46:50 +0200

    script2

--- script_reseau.js
+++ script_reseau.js
@@ -1,5 +1,35 @@
+
+/*supprimer du reseau*/
 $(document).ready(function(){
     $(".but").click(function(){
         $("#waouf").remove();
     });  
 });
+
+$(function(){
+	$(".yala1").click(function() {
+		$(".yala1").remove();
+	});
+});
+$(function(){
+	$(".yala2").click(function() {
+		$(".yala2").remove();
+	});
+});
+$(function(){
+	$(".yala3").click(function() {
+		$(".yala3").remove();
+	});
+});
+$(function(){
+	$(".yala4").click(function() {
+		$(".yala4").remove();
+	});
+});
+$(function(){
+	$(".yala5").click(function() {
+		$(".yala5").remove();
+	});
+});
+
+
