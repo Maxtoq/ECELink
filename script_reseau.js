3f9efc7d81419a1741df433b5e3bc7053304f382
Thomas WUHRLIN <thomas.wuhrlin@edu.ece.fr>
2018-05-03 19:13:57 +0200

    reseau_finish

--- script_reseau.js
+++ script_reseau.js
@@ -1,34 +1,49 @@
 
 /*supprimer du reseau*/
 $(document).ready(function(){
-    $(".but").click(function(){
-        $("#waouf").remove();
+    $(".but1").click(function(){
+        $("#waouf1").remove();
+    });  
+});
+$(document).ready(function(){
+    $(".but2").click(function(){
+        $("#waouf2").remove();
+    });  
+});
+$(document).ready(function(){
+    $(".but3").click(function(){
+        $("#waouf3").remove();
     });  
 });
 
 $(function(){
 	$(".yala1").click(function() {
 		$(".yala1").remove();
+		alert("invitation envoyée");
 	});
 });
 $(function(){
 	$(".yala2").click(function() {
 		$(".yala2").remove();
+		alert("invitation envoyée");
 	});
 });
 $(function(){
 	$(".yala3").click(function() {
 		$(".yala3").remove();
+		alert("invitation envoyée");
 	});
 });
 $(function(){
 	$(".yala4").click(function() {
 		$(".yala4").remove();
+		alert("invitation envoyée");
 	});
 });
 $(function(){
 	$(".yala5").click(function() {
 		$(".yala5").remove();
+		alert("invitation envoyée");
 	});
 });
 
