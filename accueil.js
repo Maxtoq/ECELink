e7c67c91dcfdbd6ee418b7e6c6878008c2ec5997
DESKTOP-K0CUJTA\cleme <clementpecresse@hotmail.fr>
2018-05-04 20:23:51 +0200

    accueil avec un js

--- accueil.js
+++ accueil.js
@@ -2,7 +2,7 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 function myFunction2(element) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+  var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     element.value ='';
     for (i = 0; i < dropdowns.length; i++) {
