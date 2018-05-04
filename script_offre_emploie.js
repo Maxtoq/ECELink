6f6e5ceba34ba42aa70fccd19f3d24d5c6f4858b
Thomas WUHRLIN <thomas.wuhrlin@edu.ece.fr>
2018-05-04 19:16:19 +0200

    offre emploi js

---
+++ script_offre_emploie.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+function myFunction2(element) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    element.value ='';
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
