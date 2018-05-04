e9be079d547d97f1c2d252ffe8aa3a1d3e37b5c3
Maxtoq <maxime.toquebiau@hotmail.fr>
2018-05-04 12:48:33 +0200

    bug ?

---
+++ page_accueil.js
@@ -0,0 +1,11 @@
+var publi = getElementByid("murpubli");
+function publi()
+      {
+        var $newdiv1 = $( "<div id='object1'></div>" );
+      var newdiv2 = document.getElementById("murTampon").innerHTML;
+      existingdiv1 = document.getElementById( "foo" );
+      alert(newdiv2)
+
+      newdiv2.prependTo($("#mur"));
+      }
+      
