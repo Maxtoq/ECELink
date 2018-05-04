775e926baa79e5c9c87f0adf083853d9285e1b9e
DESKTOP-K0CUJTA\cleme <clementpecresse@hotmail.fr>
2018-05-04 19:42:53 +0200

    javascript for accueil

--- accueil.js
+++ accueil.js
@@ -1,38 +1,14 @@
-var publi = getElementByid("murpubli");
-function publi()
-      {
-        var $newdiv1 = $( "<div id='object1'></div>" );
-      var newdiv2 = document.getElementById("mur").innerHTML;
-      alert(newdiv2)
-
-      newdiv2.^prependto("#mur");
-      }
-
-$(document).ready(function(){
-  function bascule(id)
-  {
-    alert("yes");
-    if (document.getElementById(id).style.visibility == "hidden")
-        document.getElementById(id).style.visibility = "visible";
-    else	document.getElementById(id).style.visibility = "hidden";
-  }
-});
-
-
-$(document).ready(function(){
 function myFunction() {
-    alert("yes");
     document.getElementById("myDropdown").classList.toggle("show");
-};
-};
-
-function myFunction2() {
+}
+function myFunction2(element) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
+    element.value ='';
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
-  };
+  }
