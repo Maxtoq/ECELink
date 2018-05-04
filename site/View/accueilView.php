24462108d72065357d1c659c4cdeff9a6e8da5ba
DESKTOP-K0CUJTA\cleme <clementpecresse@hotmail.fr>
2018-05-04 22:56:00 +0200

    suite affichage

--- site/View/accueilView.php
+++ site/View/accueilView.php
@@ -3,8 +3,8 @@
 
 <!-- Stylesheet et scripts à inclure -->
 <?php ob_start(); ?>
-<link rel="stylesheet" type = "text/css" href ="acceuil.css">
-<script type="text/javascript" src ="View/connexion.js"></script>
+<link rel="stylesheet" type = "text/css" href ="css/acceuil.css">
+<script type="text/javascript" src ="js/accueil.js"></script>
 <?php $include = ob_get_clean(); ?>
 <?php ob_end_flush(); ?>
 
