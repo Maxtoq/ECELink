cc1b4f39c5b1171df353ea2f8acbd9b25bc128c5
Maxtoq <maxime.toquebiau@hotmail.fr>
2018-05-05 11:02:22 +0200

    + goPage

--- site/index.php
+++ site/index.php
@@ -5,6 +5,8 @@ session_start();
 // On inclut les fichiers nécessaires
 require('Controler/controler.php');
 
+$controler = null;
+
 // Si les superglobales ne sont pas créées, on les crée et on les serialize
 if (!isset($_SESSION['controler'])) {
     $controler = new Controler;
@@ -15,35 +17,57 @@ else {
     $controler = unserialize($_SESSION['controler']);
 }
 
+/**
+ * Charge la page demandée en paramètre
+ *
+ * @param  string $page page demandée
+ */
+function goPage($page) {
+    global $controler;
+    // On serialize le controler avant de charger une autre page
+    $_SESSION['controler'] = serialize($controler);
+    // On charge la page demandée
+    switch ($page) {
+        case 'ACCUEIL':
+            $controler->getAccueil();
+            break;
+        case 'LOGIN':
+            $controler->getLogIn();
+            break;
+    }
+}
+
+
+
 if (isset($_GET['action'])) {
     if ($_GET['action'] == 'accueil') {
-        // On récupère les inputs du formulaire
-        $pseudo = isset($_POST["pseudo"]) ? $_POST["pseudo"] : "";
-    	$mdp = isset($_POST["mdp"]) ? $_POST["mdp"] : "";
-    	if (($pseudo != "") && ($mdp != "")) {
-            // On tente de se connecter avec les identifiants
-            // Si la connexion est réussie, on met connected à true, sinon à false
-            $controler->tryLogIn($pseudo, $mdp);
+        // Si on est déjà connecté
+        if ($controler->getConnected()) {
+            // On charge la page d'accueil
+            goPage('ACCUEIL');
+        }
+        // Sinon, on essaie de se connecter avec les identifiants
+        else {
+            // On récupère les inputs du formulaire
+            $pseudo = isset($_POST["pseudo"]) ? $_POST["pseudo"] : "";
+        	$mdp = isset($_POST["mdp"]) ? $_POST["mdp"] : "";
+        	if (($pseudo != "") && ($mdp != "")) {
+                // On tente de se connecter avec les identifiants
+                // Si la connexion est réussie, on met connected à true, sinon à false
+                $controler->tryLogIn($pseudo, $mdp);
 
-            // On serialize le controler avant de charger une autre page
-            $_SESSION['controler'] = serialize($controler);
-            // Si la connexion est réussie, on lance la page d'accueil
-            if ($controler->getConnected()) $controler->getAccueil();
-            // Sinon, on relance la page de connexion
-            else  $controler->getLogIn();
-    	}
-    	else {
-            // On serialize le controler avant de charger une autre page
-            $_SESSION['controler'] = serialize($controler);
-            $controler->getLogIn();
-    	}
-
-
+                // Si la connexion est réussie, on lance la page d'accueil
+                if ($controler->getConnected()) goPage('ACCUEIL');
+                // Sinon, on relance la page de connexion
+                else  goPage('LOGIN');
+        	}
+        	else {
+                goPage('LOGIN');
+        	}
+        }
     }
 }
 else {
-    // On serialize le controler avant de charger une autre page
-    $_SESSION['controler'] = serialize($controler);
-    $controler->getLogIn();
+    goPage('LOGIN');
 }
 ?>
